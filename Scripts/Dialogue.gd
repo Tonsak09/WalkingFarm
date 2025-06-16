73319b3e8329041f79c7c69655cf1869175371b0
Narai Risser <43816475+Tonsak09@users.noreply.github.com>
2025-06-15 21:09:33 -0400

    Better dialogue

--- Scripts/Dialogue.gd
+++ Scripts/Dialogue.gd
@@ -2,13 +2,32 @@ extends Node3D
 
 @export var label : Label3D
 @export var timer : Timer
-@export var text : String
+@export var text : Array[String] 
 
 var isTyping : bool 
+var currLine : int 
 var currChar : int 
 
+func _ready() -> void:
+	currLine = text.size(); 
+
+func _process(delta: float) -> void:
+	#print_debug( str(currLine) + " " + str(isTyping))
+	timer.paused = !isTyping
+
 func Interact():
-	StartTyping(); 
+	#print_debug(currLine and !isTyping)
+	
+	if !isTyping and currLine < text.size():
+		label.text = ""
+		currLine += 1
+		currChar = 0
+		isTyping = true
+	elif isTyping:
+		isTyping = false 
+		label.text = text[currLine]
+	else:
+		StartTyping(); 
 
 # Initializes the type system 
 func StartTyping():
@@ -17,16 +36,27 @@ func StartTyping():
 	
 	label.text = ""
 	currChar = 0
+	currLine = 0
 	timer.start()
 	isTyping = true
 
 # Adds the next character to the label text 
 func NextChar():
-	if currChar >= text.length():
+	
+	if !isTyping:
+		return
+	
+	# End of all dialogue 
+	if currLine >= text.size():
+		isTyping = false; 
+		return 
+	
+	# End of current line 
+	if currChar >= text[currLine].length():
 		isTyping = false
 		return
 	 
-	label.text += text[currChar]
+	label.text += text[currLine][currChar]
 	currChar += 1
 
 func Cleanup():
